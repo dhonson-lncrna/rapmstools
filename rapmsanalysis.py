2a84f3de7dd9ba31cb9f4bd73d5e5abdad9387db
Drew Honson <dhonson@lncrna.caltech.edu>
2022-03-25 20:51:25 -0700

    All anova coding

---
+++ rapmsanalysis.py
@@ -0,0 +1,159 @@
+import numpy as np
+import pandas as pd
+from scipy.stats import f_oneway, tukey_hsd
+import numba
+
+def crapome_filter(df, 
+                   spec='mouse'):
+    '''Removes common background from RAP-MS data'''
+    
+    # List of some common background. Capitalize for human samples
+    background = ('Krt','Dsp','Act','TUB','Tub','Lyz','Jup','nan')
+    if spec == 'human':
+        background = [i.upper() for i in background[:-1]]
+        background.append('nan')
+    else:
+        pass
+    
+    # Filter out annotated contaminants
+    df = df[~df['Protein IDs'].str.contains('CON')]
+    
+    # Filter out other known background
+    for i in background:
+        df = df[~df['Gene names'].str.contains(i)]
+        
+    return df
+
+def import_rap(path,
+               feature,
+               targets,
+               reps,
+               spec):
+    '''Imports RAP-MS data and filters contaminants'''
+    
+    # Make lists for the columns to keep
+    keep = ['Protein IDs','Gene names']
+    
+    reps = np.array(np.arange(reps) + 1, dtype=str)
+    samples = np.array(np.meshgrid(targets, reps)).T.reshape(-1,2)
+    samples = ['_'.join(s) for s in samples]
+    samples = [feature + ' ' + s for s in samples]
+    
+    keep = keep + samples
+    
+    # Import the data
+    data = pd.read_csv(path, sep='\t', usecols=keep)
+    data = data.astype({'Gene names':str})
+    
+    # Filter crapome
+    data = crapome_filter(data, spec=spec)
+    data = data.drop('Protein IDs',axis=1)
+    
+    # If column selection removed some samples, remove proteins that no longer show up
+    data['Sum'] = np.sum(data.loc[:,keep[2:]], axis=1)
+    data = data[data['Sum'] > 0]
+    data = data.drop('Sum', axis=1)
+    
+    return data
+
+def rap_anova(data,
+              reps,
+              thresh=0.05):
+    '''Performs ANOVA for all factors in a dataset imported with import_rap'''
+    # Set the index to Gene names
+    data = data.set_index('Gene names')
+    
+    # Rename the columns for easier splitting
+    data.columns = [i.split(' ')[-1] for i in data.columns]
+    
+    # Identify the individual RNAs and create a dictionary for indexing
+    rnas = np.unique([i.split('_')[0] for i in data.columns])
+    
+    # It's easier to build the sample names from scratch than to pull them from the columns
+    # to prevent key-value pairing errors
+    reps = np.array(np.arange(reps) + 1, dtype=str)
+    samples = []
+    for r in rnas:
+        samp_temp = np.array(np.meshgrid(r,reps)).T.reshape(-1,2)
+        samples.append(['_'.join(i) for i in samp_temp])
+    
+        
+    rnadict = dict(zip(rnas,samples))
+    
+    # Add a column to store p-values
+    data['OneWay_Pval'] = np.zeros(len(data))
+    
+    # Perform ANOVA for each factor
+    for i in data.index:
+        subvals = [list(data.loc[i, rnadict[k]]) for k in rnas]
+        data.loc[i,'OneWay_Pval'] = f_oneway(*subvals)[1]
+        
+    if thresh == False:
+        return data
+    
+    else:
+        return data[data['OneWay_Pval'] <= thresh]
+    
+def rap_tukey(anova,
+              reps,
+              fname,
+              thresh=0.05):
+    '''Performs Tukey test on all factors in a DataFrame output from rap_anova.
+    Returns the log10-transformed p-value for RNAs that passed the threshold or 0 for those 
+    that did not'''
+    # Log transform threshold for later calculation
+    thresh = np.log10(thresh)
+    
+    # Identify the individual RNAs and create a dictionary for indexing
+    anova = anova.drop('OneWay_Pval', axis=1)
+    rnas = np.unique([i.split('_')[0] for i in anova.columns])
+    
+    # It's easier to build the sample names from scratch than to pull them from the columns
+    # to prevent key-value pairing errors
+    reps = np.array(np.arange(reps) + 1, dtype=str)
+    samples = []
+    for r in rnas:
+        samp_temp = np.array(np.meshgrid(r,reps)).T.reshape(-1,2)
+        samples.append(['_'.join(i) for i in samp_temp])
+        
+    rnadict = dict(zip(rnas,samples))
+    
+    # Make a new DataFrame to contain the values
+    tukls = [np.zeros(len(anova)) for r in rnas] + [list(anova.index)]
+    
+    tukdf = pd.DataFrame(dict(zip(np.append(rnas, 'Gene names'), tukls)))
+    tukdf = tukdf.set_index('Gene names')
+    
+    # Perform Tukey test for each factor
+    for i in anova.index:
+        subvals = [list(anova.loc[i,rnadict[k]]) for k in rnas]
+        tukey = np.log10(tukey_hsd(*subvals).pvalue)
+        
+        # Remove self correlation (p = 1)
+        tukey = np.sort(tukey)[:,:-1]
+        
+        # Calculate median p-values
+        tmed = np.median(tukey, axis=1)
+        
+        # Determine which factors pass threshold in all pairwise tests
+        tbool = tukey <= thresh
+        tall = tbool.all(axis=1)
+        
+        # Update the dataframe
+        row = np.zeros(len(tall))
+        for j, v in enumerate(tall):
+            if v == False:
+                pass
+            else:
+                row[j] = tmed[j]
+                
+        tukdf.loc[i,:] = row
+        
+    tukdf.to_excel(fname)
+        
+    return tukdf
+        
+        
+    
+    
+    
---
+++ rnpdicts.py
@@ -0,0 +1,141 @@
+# List of RNA targets
+targets = ('XIST', 
+           '7SL',
+           'RMRP',
+           'U7',
+           '7SK',
+           'U2',
+           'U6',
+           'U1',
+           'GO Splice')
+
+# Lists of known interactors
+# Xist, from McHugh et al, Nature 2015 (https://doi.org/10.1038/nature14443)
+xist = ('Spen',
+       'Rbm15',
+       'Myef2',
+       'Celf1',
+       'Hnrnpc',
+       'Lbr',
+       'Hnrnpu',
+       'Raly',
+       'Hnrnpm',
+       'Ptbp1')
+
+# 7SL, from Luirink and Sinning, BBA-MCR 2004 (https://doi.org/10.1016/j.bbamcr.2004.03.013)
+sevensl = ('Srp9',
+           'Srp14',
+           'Srp19'
+           'Srp68',
+           'Srp72',
+           'Srp54',
+           'Srpr')
+
+# RMRP, from Jarrous, Trends in Genetics 2017 (https://doi.org/10.1016/j.tig.2017.06.006)
+# I could not find a list of all the mouse genes, so these are human
+rmrp = ('Pop1',
+        'Pop4'
+       'Rpp38',
+       'Rpp29',
+       'Rpp21',
+       'Rpp40',
+       'Rpp14',
+       'Pop5',
+       'Rpp30',
+       'Rpp25',
+       'Rpp20')
+
+# U7, from Schumperli and Pillai, CMLS 2004 (https://doi.org/10.1007/s00018-004-4190-0)
+# Lsm10 and Lsm11 are specific to U7
+useven = ('Lsm10',
+         'Lsm11',
+         'Snrpf',
+         'Snrpb',
+         'Snrpd3',
+         'Snrpg',
+         'Snrpe',
+         'Zfp100',
+         'Slbp')
+
+# 7SK, from Brogie and Price, Nucleic Acids Research 2017 (doi: 10.1093/nar/gkx262)
+# Also from Barrandon et al, MCM 2007 (https://doi.org/10.1128/MCB.00975-07)
+sevensk = ('Hexim',
+          'Mepce',
+          'Larp7',
+          'Hnrnpq',
+          'Hnrnpr',
+          'Hnrnpa1',
+          'Hnrnpa2',
+          'Cdk9',
+          'Cyct1')
+
+# U2, from Zhang et al, Nature 2020 (https://doi.org/10.1038/s41586-020-2344-3)
+# Also Scofield and Lynch, Mol Bio Evol 2008 (doi:10.1093/molbev/msn175)
+utwo = ('Snrpa1',
+       'Snrpb2',
+       'Sf3a3',
+       'Sf3b3',
+       'Sf3b1',
+       'Prp5',
+       'Sf3b6',
+       'Tat-sf1',
+       'Sf3b4',
+       'Sf3a2',
+       'Sf3a1',
+       'Snrpb',
+       'Snrpd1',
+       'Snrpd2',
+       'Snrpd3'
+       'Snrpg',
+       'Snrpe',
+       'Snrpf',
+       'Snrpg')
+
+# U6, Montemayor et al, Nature Comms 2018 (https://doi.org/10.1038/s41467-018-04145-4)
+usix = ('Lsm8', # Lsm8 is nearly identical to Lsm1, so may be some ambiguity based on recovered peptides
+       'Lsm2',
+       'Lsm3',
+       'Lsm6',
+       'Lsm5',
+       'Lsm7',
+       'Lsm4',
+       'Prp24', # contains Rrm1-4
+       'Pat1',
+       'Prpf8',
+       'Prpf3',
+       'Rbm22')
+
+# U1, Kondo et al, eLife 2015 (https://doi.org/10.7554/eLife.04986)
+uone = ('Snrpc',
+       'Snrpa',
+       'Snrnp70',
+       'Snrpb',
+       'Snrpd1',
+       'Snrpd2',
+       'Snrpd3'
+       'Snrpg',
+       'Snrpe',
+       'Snrpf',
+       'Snrpg')
+
+# Gene ontology splicing terms
+gosplice = pd.read_csv('GO_term_summary_20220227_151146.csv',usecols=['Symbol'])
+gosplice = list(gosplice['Symbol'])
+
+ms_canon_rnps = (xist, 
+              sevensl, 
+              rmrp, 
+              useven, 
+              sevensk, 
+              utwo, 
+              usix, 
+              uone,
+              gosplice)
+
+hu_canon_rnps = []
+
+for i in ms_canon_rnps:
+    hu_canon_rnps.append([u.upper() for u in i])
+
+mousernps = dict(zip(targets, ms_canon_rnps))
+humanrnps = dict(zip(targets, hu_canon_rnps))
